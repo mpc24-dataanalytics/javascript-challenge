f962df30fa3c8529fb7efd215623ccf1971a4a15
mpc24-dataanalytics <50643569+mpc24-dataanalytics@users.noreply.github.com>
2020-07-14 21:36:08 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// display UFO sightings
+function tableDisplay(ufoSightings) {
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    ufoSightings.forEach((ufoRecord) => {
+      var row = tbody.append("tr");
+      Object.entries(ufoRecord).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.html(value);
+      });
+    });
+  };
+  
+  tableDisplay(tableData);
+
+  //create filter on date
+  function datefilter (){
+    var date=d3.select ("#datetime").property("value");
+    filterdata = tableData.filter (row => row.datetime== date);
+    console.log(filterdata);
+    tableDisplay(filterdata);
+  }
+  
+  d3.selectAll("#filter-btn").on("click",datefilter);
+
+  
+
